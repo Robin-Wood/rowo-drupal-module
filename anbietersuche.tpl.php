7dabfa4b395b6baa94bfc9111cd0667ef497984f
Tim Christensen <tim.christensen@robinwood.de>
2020-01-02 09:20:51 +0100

    search

--- anbietersuche.tpl.php
+++ anbietersuche.tpl.php
@@ -1,6 +1,6 @@
 <div class="row">
   <div class="col-xs-12 col-sm-8 col-sm-offset-2">
-    <div class="v-search">
+    <div id="app" class="v-search" data-providerdata="<?php print $variables['data'] ?>" data-criteriadata="<?php print $variables['criteria'] ?>">
       <label>
         <input class="form-text" type="text" placeholder="z. B. Anbietername, Website oder Stadt">
       </label>
